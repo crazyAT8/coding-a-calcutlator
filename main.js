20d28a10bb30d3350640c69e15b57d4245ee40e4
crazyAT8 <80292649+crazyAT8@users.noreply.github.com>
2021-06-22 00:53:36 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const display1E1 =document.querySelector('.display-1');
+const display2E1 =document.querySelector('.display-2');
+const tempResultE1 =document.querySelector('.temp-result');
+const numbersE1 = document.querySelectorAll('.number');
+const operationE1 = document.querySelectorAll('.operation');
+const equalE1 = document.querySelector('.equal');
+const clearE1 = document.querySelector('..all-clear');
+const clearLastE1 = document.querySelector('.last-entity-clear');
+
+let dis1Num = "";
+let dis2Num = "";
+let result = null;
+let lastOperation = "";
+let haveDot = false;
